c29bc1a8be61028c58f7b083f57e9f2488e8cf80
Danilo Tuler <danilo.tuler@cartesi.io>
2022-12-08 11:16:24 -0300

    feat: link to home from header

--- apps/frontend/src/app/Header.tsx
+++ apps/frontend/src/app/Header.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { HStack, Spacer } from "@chakra-ui/react";
+import Link from "next/link";
 import Image from "next/image";
 import { FC } from "react";
 import Wallet from "./Wallet";
@@ -8,7 +9,9 @@ import Wallet from "./Wallet";
 const Header: FC = () => {
     return (
         <HStack w="80%" justifyContent="flex-end">
-            <Image src="/title.svg" width={240} alt="logo" height={10} />
+            <Link href="/">
+                <Image src="/title.svg" width={240} alt="logo" height={10} />
+            </Link>
             <Spacer />
             <Wallet />
         </HStack>
