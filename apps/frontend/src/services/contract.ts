d68929b9b1f0a4df18fce859a604ecb094ea6cce
Danilo Tuler <danilo.tuler@cartesi.io>
2022-12-09 11:44:55 -0300

    feat: deploy to goerli

--- apps/frontend/src/services/contract.ts
+++ apps/frontend/src/services/contract.ts
@@ -4,7 +4,7 @@ import { useNetwork } from "wagmi";
 import { useEffect, useState } from "react";
 
 const contract: Record<number, `0x${string}`> = {
-    5: "0xE8dc6065B256c14A4274498F75c57ba1b37e9659",
+    5: "0xb2C831D9803D62220E451C7026ef73A685d9646c",
     31337: "0xF8C694fd58360De278d5fF2276B7130Bfdc0192A",
 };
 
