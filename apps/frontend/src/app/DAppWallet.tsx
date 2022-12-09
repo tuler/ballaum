41cb253ecf48c3266ac1269083e3c38888606b44
Danilo Tuler <danilo.tuler@cartesi.io>
2022-12-09 00:36:35 -0300

    fix: svg

--- apps/frontend/src/app/DAppWallet.tsx
+++ apps/frontend/src/app/DAppWallet.tsx
@@ -37,9 +37,9 @@ const DAppWallet: FC<DAppWalletProps> = ({ dapp }) => {
             <path
                 d="M12.75 1.54001L8.51647 5.0038C7.77974 5.60658 6.72026 5.60658 5.98352 5.0038L1.75 1.54001"
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2.5"
                 xmlns="http://www.w3.org/2000/svg"
             ></path>
         </svg>
